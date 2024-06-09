c4a11042776c288fa5772a01ab96bc813e6ba21c
sharmatushar14 <sharmatusharofficial14@gmail.com>
2024-06-09 13:16:30 +0530

    Security Patch

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-	apiKey: "AIzaSyA4dQ949oZeMC_fg7xBEhq_JZuieL8PBgo",
-	authDomain: "unipool-mychat.firebaseapp.com",
-	projectId: "unipool-mychat",
-	storageBucket: "unipool-mychat.appspot.com",
-	messagingSenderId: "555418255779",
-	appId: "1:555418255779:web:213a281032f32b4b7eda16",
-	measurementId: "G-1D0W7RQG52"
+
   };
 
 export default firebaseConfig
