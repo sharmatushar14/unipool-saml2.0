bd5ff4f0e86edfe490814b3426c886aafb9b6bc0
sharmatushar14 <sharmatusharofficial14@gmail.com>
2024-06-11 15:03:33 +0530

    Fixing final bugs before production shifting

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHD,
+    projectId: process.env.REACT_APP_FIREBASE_PID,
+    storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MID,
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
+  export const db = getFirestore(app);
+ 
